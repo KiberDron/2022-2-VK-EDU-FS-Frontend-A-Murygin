2d3daa7e2c926bf9238face4fba591c66a9db030
KiberDron <andreynotebook@gmail.com>
2022-11-19 00:16:00 +0300

    small fix

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
-import { HashRouter as Router, Routes, Route, HashRouter } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import PageChatList from './pages/PageChatList/PageChatList'
 import PageChat from './pages/PageChat/PageChat'
 import PageProfile from './pages/PageProfile/PageProfile'
@@ -9,13 +9,13 @@ import PageProfile from './pages/PageProfile/PageProfile'
 class App extends Component {
 	render() {
 		return (
-			<HashRouter>
+			<Router>
 				<Routes>
 					<Route path='/' element={<PageChatList/>}/>
 					<Route path='/chat' element={<PageChat/>}/>
 					<Route path='/profile' element={<PageProfile/>}/>
 				</Routes>
-			</HashRouter>
+			</Router>
 		);
 	}
 }
