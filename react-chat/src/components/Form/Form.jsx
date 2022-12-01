78655c4412da1aa9b148610e37c4755bd365796f
KiberDron <andreynotebook@gmail.com>
2022-12-01 23:53:49 +0300

    added location, audio buttons

--- react-chat/src/components/Form/Form.jsx
+++ react-chat/src/components/Form/Form.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import classes from './Form.module.scss'
 import Attachment from '@mui/icons-material/Attachment';
+import ShareLocation from '@mui/icons-material/ShareLocation';
+import Mic from '@mui/icons-material/Mic';
 
 
 export default function Form({onSubmit, ...props}) {
     return (
         <form onSubmit={onSubmit} className={classes.form}>
-            <input className={classes.form_input} 
-            {...props}/>
             <button className={classes.attachment_button}>
                 <Attachment></Attachment>
+            </button>
+            <input className={classes.form_input} 
+            {...props}/>
+            <button className={classes.location_button}>
+                <ShareLocation></ShareLocation>
+            </button>
+            <button className={classes.audio_button}>
+                <Mic></Mic>
             </button>
         </form>
     )
