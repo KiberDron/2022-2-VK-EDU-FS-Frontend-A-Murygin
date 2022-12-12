18975a71771d95148cd3835b9afb828ea1145b6a
KiberDron <andreynotebook@gmail.com>
2022-12-12 11:11:22 +0300

    created login page

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -5,8 +5,10 @@ import PageChatList from './pages/PageChatList/PageChatList'
 import PageChat from './pages/PageChat/PageChat'
 import PageGlobalChat from './pages/PageChat/PageGlobalChat'
 import PageProfile from './pages/PageProfile/PageProfile'
+import PageLogin from './pages/PageLogin/PageLogin'
+import { gapi } from 'gapi-script';
 
-
+//console.log(gapi.auth2.getAuthInstance().isSignedIn.get())
 class App extends Component {
 	render() {
 		return (
@@ -15,7 +17,7 @@ class App extends Component {
 					<Route path='/' element={<PageChatList/>}/>
 					<Route path='/chat' element={<PageChat/>}/>
 					<Route path='/global_chat' element={<PageGlobalChat/>}/>
-					<Route path='/profile' element={<PageProfile/>}/>
+					<Route path='/profile' element={<PageLogin/>}/>
 				</Routes>
 			</Router>
 		);
