34db4553db6f912417beecdaef14e8da1f4f8e40
KiberDron <andreynotebook@gmail.com>
2022-12-12 19:52:31 +0300

    configured authorization via google

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -6,18 +6,32 @@ import PageChat from './pages/PageChat/PageChat'
 import PageGlobalChat from './pages/PageChat/PageGlobalChat'
 import PageProfile from './pages/PageProfile/PageProfile'
 import PageLogin from './pages/PageLogin/PageLogin'
-import { gapi } from 'gapi-script';
 
-//console.log(gapi.auth2.getAuthInstance().isSignedIn.get())
+
 class App extends Component {
 	render() {
+		function isLogin() { // запрос на инфо юзера использует LoginRequiredMixin на бэке, поэтому может служить проверкой залогиненности
+			const request = new XMLHttpRequest();
+			request.open('GET', 'api/users/4', false);  // `false` чтобы сделать запрос синхронным
+			request.send(null);
+			let response;
+			try {
+				response = JSON.parse(request.response)
+			} catch(err) {
+				return
+			}
+			 
+			//console.log(response)
+			return response
+		}
+
 		return (
 			<Router>
 				<Routes>
-					<Route path='/' element={<PageChatList/>}/>
-					<Route path='/chat' element={<PageChat/>}/>
-					<Route path='/global_chat' element={<PageGlobalChat/>}/>
-					<Route path='/profile' element={<PageLogin/>}/>
+					<Route path='/' element={isLogin() ? <PageChatList/> : <PageLogin/>}/>
+					<Route path='/chat' element={isLogin() ? <PageChat/> : <PageLogin/>}/>
+					<Route path='/global_chat' element={isLogin() ? <PageGlobalChat/> : <PageLogin/>}/>
+					<Route path='/profile' element={isLogin() ? <PageProfile/> : <PageLogin/>}/>
 				</Routes>
 			</Router>
 		);
